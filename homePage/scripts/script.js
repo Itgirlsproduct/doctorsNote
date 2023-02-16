d26d4e1b92cb3cb62e50db90f819643bde827718
Ekaterina-2022 <davydovakaterina2022@gmail.com>
2023-02-16 17:24:49 +0300

    docs: add first code in html, script and styles

---
+++ homePage/scripts/script.js
@@ -0,0 +1,34 @@
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+const sidebar = document.getElementById("sidebar");
+
+const openSidebar = () => {
+	sidebar.style.display = "inline-block";
+};
+
+const closeSidebar = () => {
+	sidebar.style.display = "none";
+};
+
+// Event listeners
+open.addEventListener("click", openSidebar);
+close.addEventListener("click", closeSidebar);
+
+// Smooth scrolling
+$(document).ready(function () {
+	$("ul li a").on("click", function (event) {
+		if (this.hash !== "") {
+			event.preventDefault();
+			var hash = this.hash;
+			$("html, body").animate(
+				{
+					scrollTop: $(hash).offset().top,
+				},
+				600,
+				function () {
+					window.location.hash = hash;
+				}
+			);
+		}
+	});
+});
